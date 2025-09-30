4ee0cfc14819cf77fea592f274839678aac0a93a
Dino <dinodinac2003@gmail.com>
2025-09-30 18:26:19 +0200

    Add About Me Page

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -1,53 +1,69 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
+import StackIcon from "tech-stack-icons";
 const GridLayout = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <div className="grid grid-cols-4 gap-3 w-full h-full p-[100px]">
-        <div
-          className="col-span-2 row-span-2 bg-yellow-500 min-h-[100px] opacity-60
-                        transition-transform duration-300 ease-in-out hover:scale-101
-                        hover:opacity-100 rounded-sm relative"
-        ></div>
-        <div
-          className="col-span-2 bg-orange-500 min-h-[100px] opacity-60
-        transition-transform duration-300 ease-in-out hover:scale-101 overflow-hidden
-        hover:opacity-100 rounded-sm relative" 
-        >
-          <img
-            className="absolute inset-0 w-full h-full object-cover"
-            src="/images/mern.png"
-            alt="Tech Stack"
-          />
+    <>
+      <div className="flex w-screen h-screen">
+        <div className="flex justify-center items-start flex-col gap-3 w-1/2 p-[20vh] text-start">
+          <h1 className="text-3xl font-bold mb-2">About me</h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            consequuntur consequatur natus assumenda ab cum reiciendis, ipsum
+            laudantium eligendi aut accusamus iure architecto obcaecati maxime
+            laborum tempore quasi iusto veritatis.
+          </p>
         </div>
-        <div
-          className="col-span-1 bg-blue-500 min-h-[100px] opacity-60
+        <div className="flex justify-center items-center w-1/2">
+          <div className="grid grid-cols-2 gap-3 w-full p-[20vh]">
+            <div
+              className="grid-item col-span-1 aspect-[1/1] opacity-60
+                        transition-transform duration-300 ease-in-out hover:scale-101
+                        hover:opacity-100 rounded-lg relative flex justify-center items-center"
+            >
+              <StackIcon
+                className=""
+                name="nodejs"
+                style={{ width: "15vh", height: "15vh" }}
+              />
+            </div>
+            <div
+              className="grid-item col-span-1 aspect-[1/1] opacity-60
+        transition-transform duration-300 ease-in-out hover:scale-101 overflow-hidden
+        hover:opacity-100 rounded-lg relative flex justify-center items-center"
+            >
+              <StackIcon
+                className=""
+                name="react"
+                style={{ width: "15vh", height: "15vh" }}
+              />
+            </div>
+            <div
+              className="grid-item col-span-1 aspect-[1/1] opacity-60
         transition-transform duration-300 ease-in-out hover:scale-101
-        hover:opacity-100 rounded-sm relative"
-        ></div>
-        <div
-          className="col-span-1 bg-pink-500 min-h-[100px] opacity-60
+        hover:opacity-100 rounded-lg relative flex justify-center items-center"
+            >
+              <StackIcon
+                className=""
+                name="mongodb"
+                style={{ width: "15vh", height: "15vh" }}
+              />
+            </div>
+            <div
+              className="grid-item col-span-1 aspect-[1/1] opacity-60
         transition-transform duration-300 hover:scale-101
-        hover:opacity-100 rounded-sm relative"
-        ></div>
-        <div
-          className="col-span-2 bg-green-500 min-h-[100px] opacity-60
-        transition-transform duration-300 ease-in-out hover:scale-101
-        hover:opacity-100 rounded-sm relative"
-        ></div>
-        <div
-          className="col-span-1 bg-purple-500 min-h-[100px] opacity-60
-        transition-transform duration-300 ease-in-out hover:scale-101
-        hover:opacity-100 rounded-sm relative"
-        ></div>
-        <div
-          className="col-span-1 bg-red-500 min-h-[100px] opacity-60
-        transition-transform duration-300 ease-in-out hover:scale-101
-        hover:opacity-100 rounded-sm relative"
-        ></div>
+        hover:opacity-100 rounded-lg relative flex justify-center items-center"
+            >
+              <StackIcon
+                className=""
+                name="expressjs"
+                style={{ width: "15vh", height: "15vh" }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
